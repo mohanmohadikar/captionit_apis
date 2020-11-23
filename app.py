daef2c575ef8e399d388cf88d135f8c732d64276
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2020-11-23 13:36:25 +0530

    change to tf as backend.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from flask import Flask, jsonify, request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.applications.xception import Xception
 import pickle
 
 # load model
 model = pickle.load(open('model.pkl','rb'))
+h5model = load_model(“model.h5”)
+max_length = 32
+tokenizer = pickle.load(open("tokenizer.p","rb"))
+xception_model = Xception(include_top=False, pooling="avg")
 
 # app
 app = Flask(__name__)
@@ -15,6 +23,19 @@ def predict():
     # get data
     data = request.get_json(force=True)
     print(data)
+
+###imp start.
+
+    
+
+    photo = extract_features(img_path, xception_model)
+    img = Image.open(img_path)
+
+    description = generate_desc(model, tokenizer, photo, max_length)
+    print("\n\n")
+    print(description)
+
+    ### imp end.
     
 
     # convert data into dataframe
